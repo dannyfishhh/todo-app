bded12486569fd52bd4322772ad6750be46f401b
Danny Fish <dannyfishhh@gmail.com>
2025-02-03 12:06:41 +0000

    break for lunch

--- src/components/addTodoForm/addTodoForm.js
+++ src/components/addTodoForm/addTodoForm.js
@@ -1,7 +1,27 @@
+import {addTodo} from '../../store/features/todo/todoSlice';
+import {useDispatch} from 'react-redux';
+import {useState} from 'react';
+
 const AddTodoForm = () => {
+
+    const [input, setInput] = useState('');
+    const dispatch = useDispatch()
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (!input) {
+            alert('Set a valid todo');
+        } else {
+            dispatch(addTodo({
+                id: Math.random().toString(36).substr(2, 9),
+                name: input
+            }))
+            setInput('');
+        }
+    }
+
     return (
-        <form>
-            <input type='text' placeholder='New Todo'></input>
+        <form onSubmit={handleSubmit}>
+            <input value={input} onChange={(e) => setInput(e.target.value)} type='text' placeholder='New Todo'></input>
             <button type='submit'>
                 Add
             </button>
